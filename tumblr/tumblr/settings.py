db2be49b9983db4bb6ac1d03f77be6be03e0749a
Annie Wang <anniwang@seas.upenn.edu>
2020-12-02 00:36:29 -0800

    removed bad files

--- tumblr/tumblr/settings.py
+++ tumblr/tumblr/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u9#9okgfbo9u5tvy@q^)%^u*_1=y%+v=htk^at)ak#q%hn^(js'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
